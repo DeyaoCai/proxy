ed58fb1048c5b738e267c0d3651be08d03038b01
DeyaoCai <360867514@qq.com>
2019-03-05 15:30:18 +0800

    no message

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const http = require('http');
 const httpsToHttpDefaultPort = 443;
 const httpToHttpDefaultPort = 9990;
 
-const httpToHttp = require('./server/TemHttpToHttp');
-const httpsToHttp = require('./server/TemHttpsToHttp');
+const TemHttpToHttp = require('./server/TemHttpToHttp');
+const TemHttpsToHttp = require('./server/TemHttpsToHttp');
 
 // https 需要安全证书
 const options = {
@@ -13,11 +13,11 @@ const options = {
     cert: fs.readFileSync('./certificate.pem')
 };
 // 将 https 代理成 http
-https.createServer(options, httpsToHttp).listen(httpsToHttpDefaultPort, function () {
+https.createServer(options, TemHttpsToHttp).listen(httpsToHttpDefaultPort, function () {
     console.log('Https server listening on port ' + httpsToHttpDefaultPort);
 });
 
 // http 代理成http
-http.createServer(httpToHttp).listen(httpToHttpDefaultPort, function () {
+http.createServer(TemHttpToHttp).listen(httpToHttpDefaultPort, function () {
     console.log('Http server listening on port ' + httpToHttpDefaultPort);
 });
