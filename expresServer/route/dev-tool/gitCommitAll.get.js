4bf9929c86c38195c97ade956d2ee97b73d99a1d
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-30 17:06:40 +0800

    auto commit

--- expresServer/route/dev-tool/gitCommitAll.get.js
+++ expresServer/route/dev-tool/gitCommitAll.get.js
@@ -12,6 +12,7 @@ module.exports = function (req, res) {
     try{process.chdir(item);}catch(e){}
     try{cProcess.execSync(`git add .`);}catch(e){}
     try{cProcess.execSync(`git commit -m "auto commit"`);}catch(e){}
+    try{cProcess.execSync(`git push`);}catch(e){}
     try{process.chdir(cwd);}catch(e){}
   });
   try {
