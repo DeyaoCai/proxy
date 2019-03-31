62341dc39f13225fc10ed91e442d6dc5db2d1603
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-03-31 23:43:18 +0800

    auto commit

--- expresServer/socket/dev-tool/gitCommitAll.js
+++ expresServer/socket/dev-tool/gitCommitAll.js
@@ -10,25 +10,26 @@ module.exports = function (req, socket, option) {
   repertoryPath[0].forEach(item => {
     try{process.chdir(item);}catch(e){}
     try{
-      console.log("gitCommitAllData", `git add . ${item}`);
+      console.log(`git add . ${item}`);
       socket.emit("gitCommitAllData", `git add . ${item}`);
       cProcess.execSync(`git add .`);
     }catch(e){}
     try{
-      console.log("gitCommitAllData", `git commit -m "auto commit" ${item}`);
+      console.log(`git commit -m "auto commit" ${item}`);
       socket.emit("gitCommitAllData", `git commit -m "auto commit" ${item}`);
       cProcess.execSync(`git commit -m "auto commit"`);
     }catch(e){}
     try{
-      console.log("gitCommitAllData", `git push ${item}`);
+      console.log(`git push ${item}`);
       socket.emit("gitCommitAllData", `git push ${item}`);
       cProcess.execSync(`git push`);
     }catch(e){}
-    try{console.log("gitCommitAllData", `git push complete ${item}`);
+    try{
+      console.log(`git push complete ${item}`);
       socket.emit("gitCommitAllData", `git push complete ${item}`);
       process.chdir(cwd);
     }catch(e){}
   });
-  console.log("gitCommitAllSuccess", `git commit all complete!`);
+  console.log(`git commit all complete!`);
   socket.emit("gitCommitAllSuccess", `git commit all complete!`);
 };
