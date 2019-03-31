2c7338c3fc0d320931cedc093c8cad1118b05526
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-04-01 00:19:47 +0800

    auto commit

--- expresServer/socket/dev-tool/gitCommitAll.js
+++ expresServer/socket/dev-tool/gitCommitAll.js
@@ -9,32 +9,30 @@ module.exports = function (req, socket, option) {
   const repertoryPath = tools.getWorkspaceDirs(workspace);
   let count = repertoryPath[0].length;
   repertoryPath[0].forEach(item => {
-    setTimeout(() => {
-      try{process.chdir(item);}catch(e){}
-      try{
-        console.log(`git add . ${item}`);
-        cProcess.execSync(`git add .`);
-      }catch(e){}
-      try{
-        console.log(`git commit -m "auto commit" ${item}`);
-        cProcess.execSync(`git commit -m "auto commit"`);
-      }catch(e){}
-      try{
-        console.log(`git push ${item}`);
-        cProcess.execSync(`git push`);
-      }catch(e){}
-      try{
-        console.log(`git push complete ${item}`);
-        count --;
-        if (count <= 0) {
-          console.log(546)
-          socket.emit("gitCommitAllSuccess", `git commit all complete!`);
-        } else{
-          // socket.emit("gitCommitAllData", `git push complete ${item}`);
-        }
-        process.chdir(cwd);
-      }catch(e){}
-    },10)
+    try{process.chdir(item);}catch(e){}
+    try{
+      console.log(`git add . ${item}`);
+      cProcess.execSync(`git add .`);
+    }catch(e){}
+    try{
+      console.log(`git commit -m "auto commit" ${item}`);
+      cProcess.execSync(`git commit -m "auto commit"`);
+    }catch(e){}
+    try{
+      console.log(`git push ${item}`);
+      cProcess.execSync(`git push`);
+    }catch(e){}
+    try{
+      console.log(`git push complete ${item}`);
+      count --;
+      if (count <= 0) {
+        console.log(546)
+        socket.emit("gitCommitAllSuccess", `git commit all complete!`);
+      } else{
+        // socket.emit("gitCommitAllData", `git push complete ${item}`);
+      }
+      process.chdir(cwd);
+    }catch(e){}
   });
   console.log(`start commit all!`);
   // socket.emit("gitCommitAllData", `start commit all!`);
