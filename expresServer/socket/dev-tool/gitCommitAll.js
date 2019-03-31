158682dce3b8a4d817b39cec1c818169a21db36b
DeyaoCai <32273577+DeyaoCai@users.noreply.github.com>
2019-04-01 00:04:39 +0800

    auto commit

--- expresServer/socket/dev-tool/gitCommitAll.js
+++ expresServer/socket/dev-tool/gitCommitAll.js
@@ -7,29 +7,37 @@ module.exports = function (req, socket, option) {
   const tools = Tools(option.ctoolsOpt.devToolsDir);
   const {workspace} = req;
   const repertoryPath = tools.getWorkspaceDirs(workspace);
+  let count = repertoryPath[0].length;
   repertoryPath[0].forEach(item => {
-    try{process.chdir(item);}catch(e){}
-    try{
-      console.log(`git add . ${item}`);
-      socket.emit("gitCommitAllData", `git add . ${item}`);
-      cProcess.execSync(`git add .`);
-    }catch(e){}
-    try{
-      console.log(`git commit -m "auto commit" ${item}`);
-      socket.emit("gitCommitAllData", `git commit -m "auto commit" ${item}`);
-      cProcess.execSync(`git commit -m "auto commit"`);
-    }catch(e){}
-    try{
-      console.log(`git push ${item}`);
-      socket.emit("gitCommitAllData", `git push ${item}`);
-      cProcess.execSync(`git push`);
-    }catch(e){}
-    try{
-      console.log(`git push complete ${item}`);
-      socket.emit("gitCommitAllData", `git push complete ${item}`);
-      process.chdir(cwd);
-    }catch(e){}
+    setTimeout(() => {
+      try{process.chdir(item);}catch(e){}
+      try{
+        console.log(`git add . ${item}`);
+        // socket.emit("gitCommitAllData", `git add . ${item}`);
+        cProcess.execSync(`git add .`);
+      }catch(e){}
+      try{
+        console.log(`git commit -m "auto commit" ${item}`);
+        // socket.emit("gitCommitAllData", `git commit -m "auto commit" ${item}`);
+        cProcess.execSync(`git commit -m "auto commit"`);
+      }catch(e){}
+      try{
+        console.log(`git push ${item}`);
+        // socket.emit("gitCommitAllData", `git push ${item}`);
+        cProcess.execSync(`git push`);
+      }catch(e){}
+      try{
+        console.log(`git push complete ${item}`);
+        count --;
+        if (count <= 0) {
+          socket.emit("gitCommitAllSuccess", `git commit all complete!`);
+        } else{
+          socket.emit("gitCommitAllData", `git push complete ${item}`);
+        }
+        process.chdir(cwd);
+      }catch(e){}
+    },10)
   });
-  console.log(`git commit all complete!`);
-  socket.emit("gitCommitAllSuccess", `git commit all complete!`);
+  console.log(`start commit all!`);
+  socket.emit("gitCommitAllData", `start commit all!`);
 };
